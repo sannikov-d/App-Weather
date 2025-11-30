d1c6b41ce796a28e7268091bce9549a5612d4f4f
Dmitry Sannikov <dosannikov@gmail.com>
2025-11-30 14:49:22 +0300

    Added full logic and layouts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main-panel">
+      <header class="main-panel__header">
+        <h1>App Weather</h1>
+        <hr class="main-panel__divider"/>
+      </header>
+      <div class="main-panel__body">
+        <input v-model="currentCity" @keydown.enter="getWeather(currentCity)" class="city-input" type="text"
+               placeholder="Enter city">
+        <WeatherBlock v-show="currentTemp != 0" :Temperature="currentTemp" :CityName="resultCityName"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import WeatherBlock from "@/components/WeatherBlock.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {WeatherBlock},
+  data() {
+    return {
+      currentCity: "",
+      resultCityName: "",
+      currentTemp: 0,
+    }
+  },
+  methods: {
+    async getWeather(cityName) {
+      try {
+        const res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=e7d5d748d554330620be283d9cc5aae6`)
+        if (res.status === 200) {
+          const lat = res.data.length != 0 ? res.data[0].lat : 0;
+          const lon = res.data.length != 0 ? res.data[0].lon : 0;
+          this.resultCityName = res.data.length != 0 ? res.data[0].name : '';
+
+          await this.getWeatherByCord(lat, lon);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getWeatherByCord(lat, lon) {
+      try {
+        const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=e7d5d748d554330620be283d9cc5aae6`)
+        if (res.status === 200) {
+          this.currentTemp = Math.ceil(res.data.main.temp);
+        }
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" src="./assets/css/Main.scss"></style>
---
+++ src/components/WeatherBlock.vue
@@ -0,0 +1,25 @@
+﻿<script>
+export default {
+  name: "WeatherBlock",
+  props:{
+    Temperature: {
+      type: Number,
+      default: 0
+    },
+    CityName: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="main-panel__weather-block">
+    <h3 v-show="CityName != ''" class="main-panel__city-name">{{ CityName }}</h3>
+    <p v-show="CityName != ''" class="main-panel__temperature">Temperature: {{ Temperature }} C</p>
+    <p v-show="CityName == ''" class="main-panel__temperature">City not found</p>
+  </div>
+</template>
+
+<style lang="scss" src="../assets/css/WeatherBlock.scss"></style>
